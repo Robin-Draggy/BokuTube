8f05cff1537dc51a5465ef2435700df8370d14ba
Abdullah Al Mamun <abdullahmn3399@gmail.com>
2026-04-14 01:19:40 +0600

    added route for controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,27 +1,48 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  updateAccount,
+  updateAvatar,
+  updateCoverImage,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { jwtVerify } from "../middlewares/auth.middleware.js";
 
-export const router = Router()
+export const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        },
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ])
-    ,registerUser);
-
-
-
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 router.route("/login").post(loginUser);
 
 // SECURED ROUTES
-router.route("/logout").post(jwtVerify, logoutUser)
-router.route("/refreshToken").post(refreshAccessToken)
+router.route("/logout").post(jwtVerify, logoutUser);
+router.route("/refreshToken").post(refreshAccessToken);
+router.route("/change-password").post(jwtVerify, changeCurrentPassword);
+router.route("/current-user").get(jwtVerify, getCurrentUser);
+router.route("/update-account").patch(jwtVerify, updateAccount);
+router
+  .route("/update-avatar")
+  .patch(jwtVerify, upload.single("avatar"), updateAvatar);
+router
+  .route("/update-cover-image")
+  .patch(jwtVerify, upload.single("coverImage"), updateCoverImage);
+router.route("/c/:username").get(jwtVerify, getUserChannelProfile);
+router.route("/history").get(jwtVerify, getWatchHistory);
